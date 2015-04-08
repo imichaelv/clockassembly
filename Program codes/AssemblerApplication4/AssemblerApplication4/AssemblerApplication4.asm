ba6910f1b8cb9fbeed4c259ca75ae6ef98e797cf
imichaelv <pjvanderveen@msn.com>
2015-04-08 21:01:18 +0200

    ASM code update, Registers moeten verbeterd worden

--- Program codes/AssemblerApplication4/AssemblerApplication4/AssemblerApplication4.asm
+++ Program codes/AssemblerApplication4/AssemblerApplication4/AssemblerApplication4.asm
@@ -7,27 +7,22 @@
 
  .include "m32def.inc"					;
  
- .data hourT		= r1				; Set hour   Tens to register  1
- .data hourO		= r2				; Set hour   Ones to register  2
- .data minuteT		= r3				; Set minute Tens to register  3
- .data minuteO		= r4				; Set minute Ones to register  4
- .data secondT		= r5				; Set second Tens to register  5
- .data secondO		= r6				; Set second Ones to register  6
+ .def hour			= r16				; Set hour   Tens to register  1
+ .def minute		= r17				; Set minute Tens to register  3
+ .def second		= r18				; Set second Tens to register  5
  
- .data hourAlarmT	= r7				; Set hourAlarm   Tens to register  7
- .data hourAlarmO	= r8				; Set hourAlarm   Ones to register  8
- .data minuteAlarmT	= r9				; Set minuteAlarm Tens to register  9
- .data minuteAlarmO	= r10				; Set minuteAlarm Ones to register 10
+ .def hourAlarm		= r19				; Set hourAlarm   Tens to register  7
+ .def minuteAlarm	= r20				; Set minuteAlarm Tens to register  9
 
- .data editLevel	= r11				; Set editLevel   to register 11
- .data sw0Counter	= r12				; Set sw0Counter  to register 12
- .data sw1Counter	= r13				; Set sw1Counter  to register 13
+ .def editLevel		= r21				; Set editLevel   to register 11
+ .def sw0Counter	= r22				; Set sw0Counter  to register 12
+ .def sw1Counter	= r23				; Set sw1Counter  to register 13
 
- .data sw0Value		= r14				; Set switch 0 Value to register 14
- .data sw1Value		= r15				; Set switch 1 Value to register 15
+ .def sw0Value		= r25				; Set switch 0 Value to register 14
+ .def sw1Value		= r11				; Set switch 1 Value to register 15
 
- .data temp			= r16				; Set temp   to register 16
- .data saveSR		= r17				; Set saveSR to register 17
+ .def temp			= r24				; Set temp   to register 16
+ .def saveSR		= r12				; Set saveSR to register 17
 
 
 
@@ -46,6 +41,20 @@
 ;=======================================;
 ;#######################################;
  init:									;
+	;******initiaze starting values*****;
+	ldi hour,0xff
+	ldi minute,0xff
+	ldi second,0xff
+	ldi hourAlarm,0xff
+	ldi minuteAlarm,0xff
+	ldi editLevel,0
+	ldi sw0Counter,0x00
+	ldi sw1Counter,0x00
+	ldi temp,0x00
+	ld sw0Value,temp
+	ld sw1Value,temp
+				
+					
 										;
 	;******initiaize stack pointer******;	=============================
 	ldi temp, high(RAMEND)				;	=							=
@@ -105,13 +114,55 @@ loop:									;
 ;=======================================;
 ;#######################################;
 										;
-CLOCK_CYLCLE:							;	=============================
+CLOCK_CYCLE:							;	=============================
 	in saveSR, SREG						;	= Checks on every clock		=
-	call checkEditLevel					;	= * What Edit level its on  =
-	call showDisplay					;	= * How the Display must	=
+	rcall checkEditLevel				;	= * What Edit level its on	=
+	cpi editLevel,4						;	= * Check if the clock is	=
+	brsh incSecond						;	=	running.				=
+	rcall showDisplay					;	= * How the Display must	=
 	out SREG, saveSR					;	=	be shown				=	
 	reti								;	=============================
 										;
 ;=======================================;
 ;--------------END LABEL----------------;
 ;=======================================;
+
+
+
+
+
+;#######################################;
+;=======================================;
+;----------Check Edit Level-------------;
+;=======================================;
+;#######################################;
+										;
+checkEditLevel:							;	=============================
+	cpi editLevel,0
+	breq startup
+	cpi editLevel,1
+	breq setHour
+	cpi editLevel,2
+	breq setMinute
+	cpi editLevel,3
+	breq setSecond
+	cpi editLevel,4
+	breq setAlarmStartup
+	cpi editLevel,5
+	breq setAlarmHour
+	cpi editLevel,6
+	breq setAlarmMinute
+	cpi editLevel,7
+	breq playNoAlarm
+	cpi editLevel,8
+	breq playYesAlarm
+	cpi editLevel,9
+	breq playNoAlarmAgain
+	ret									;	=============================
+										;
+;=======================================;
+;--------------END LABEL----------------;
+;=======================================;
+
+
+
